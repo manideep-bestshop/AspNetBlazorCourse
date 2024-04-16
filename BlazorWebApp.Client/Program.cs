fca2259bc60616e7a80485933a07cd33b642be06
DotcraftInnovations <DotcraftInnovations@LAPTOP-PH67IQOQ>
2024-04-16 21:52:42 +0530

    Add project files.

---
+++ BlazorWebApp.Client/Program.cs
@@ -0,0 +1,8 @@
+using BlazorWebApp.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+await builder.Build().RunAsync();
